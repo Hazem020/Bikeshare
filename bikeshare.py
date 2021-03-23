5024a73711bc67709a565ccaf3b99807207f87f9
Hazem <59257408+Hazem020@users.noreply.github.com>
2021-03-23 15:38:28 +0200

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -155,6 +155,8 @@ def user_stats(df):
        
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+
+
 def display_data(df):
     """Displays 5 rows of data.
     Args:
